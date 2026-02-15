749da677955304dc7dd2573f3d8666a52e9e0100
djdonjuan86-coder <djdonjuan86@gmail.com>
2026-02-15 22:56:25 +0700

    fix1

--- src/app/live/page.js
+++ src/app/live/page.js
@@ -43,7 +43,7 @@ export default function LivePage() {
       </div>
 
       {/* Preview Area Fullscreen Responsif dengan Foto Blur */}
-      <div className="w-full h-[80vh] sm:h-[80vh] md:h-[500px] relative overflow-hidden rounded-xl flex items-center justify-center">
+      <div className="w-full h-[50vh] sm:h-[70vh] md:h-[500px] relative overflow-hidden rounded-xl flex items-center justify-center">
         {/* Background Foto Blur */}
         <img
           src="https://i.ibb.co.com/cKWgqXFf/image.png" // ganti dengan foto konten nyata
