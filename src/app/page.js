d63e4bf4131a51a454f8520bd6fa012bd5f853f9
djdonjuan86-coder <djdonjuan86@gmail.com>
2026-02-15 22:47:13 +0700

    fix page view error

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,30 @@
 export default function Home() {
   return (
-    <main className="min-h-screen w-full bg-gray-100 flex flex-col">
-      <div className="p-4 text-center">
-        <h1 className="text-2xl font-bold">Kids Fashion Story</h1>
-        <p className="text-sm">Stylish · Spacious · Fashionable</p>
-      </div>
+    <main className="min-h-screen bg-gray-100 flex flex-col items-center justify-center text-center p-10">
+      <h1 className="text-4xl font-bold mb-4">Kids Fashion Story</h1>
+      <p className="text-s mb-3">Stylish · Spacious · Fasyhionable</p>
+      <div className="p-4 space-y-4">
+        <h1 className="text-xl font-bold">🔴 Live Streaming</h1>
 
-      {/* Preview Full Height */}
-      <div className="relative flex-1 overflow-hidden flex items-center justify-center">
-        <img src="https://i.ibb.co/cKWgqXFf/image.png" alt="Preview Live" className="absolute inset-0 w-full h-full object-cover blur-sm" />
+        {/* Preview Area Fullscreen Responsif dengan Foto Blur */}
+        <div className="w-full h-[50vh] sm:h-[70vh] md:h-[500px] relative overflow-hidden rounded-xl flex items-center justify-center">
+          {/* Background Foto Blur */}
+          <img
+            src="https://i.ibb.co.com/cKWgqXFf/image.png" // ganti dengan foto konten nyata oke
+            alt="Preview Live"
+            className="absolute inset-0 w-full h-full object-cover filter blur-sm"
+          />
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="w-16 h-16 text-gray-300 z-10" fill="currentColor" viewBox="0 0 24 24">
-          <path d="M8 5v14l11-7z" />
-        </svg>
-      </div>
-
-      <div className="p-4">
+          {/* Icon Play Video */}
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-16 h-16 text-gray-400 opacity-50 hover:opacity-50 transition-opacity cursor-pointer z-10" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M8 5v14l11-7z" />
+          </svg>
+        </div>
         <a href="/live" className="block bg-pink-500 text-white text-center py-3 rounded-xl font-semibold">
           Join Live Sekarang
         </a>
       </div>
+      <p> </p>
     </main>
   );
 }
